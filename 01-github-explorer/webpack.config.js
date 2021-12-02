5abff637bb4646a3ede59871980d04dbfd2c7593
Derek Cardoso <derek.cardoso@hotmail.com>
2021-12-02 14:14:54 -0300

    version 0.2

--- 01-github-explorer/webpack.config.js
+++ 01-github-explorer/webpack.config.js
@@ -1,7 +1,11 @@
 const path = require('path') 
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-  mode: 'development',
+  mode: isDevelopment ? 'development' : 'production',
+  devtool: isDevelopment ? 'eval-source-map': 'source-map',
   entry: path.resolve(__dirname,'src','index.jsx'),
   output: { //aqui indica onde está o arquivo de saída da aplicação./
     path: path.resolve(__dirname,'dist'),
@@ -10,12 +14,25 @@ module.exports = {
   resolve: {
     extensions: ['.js','.jsx'] //como estou usando arquivos .jsx preciso falar para aplicação que é para ler arquivos .jsx/
   },
+  devServer: {
+    static: path.resolve(__dirname, 'public') //mostra onde está o html estático da aplicação
+  },
+  plugins: [
+    new HtmlWebpackPlugin ({
+      template: path.resolve(__dirname, 'public', 'index.html')
+    })
+  ],
   module: {
     rules: [
       {
         test: /\.jsx$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
       }
     ],
   }
